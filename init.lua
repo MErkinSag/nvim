14892f0ed14a954d6c811719d27d358ef276643d
ErkinSagiroglu <52523336+MErkinSag@users.noreply.github.com>
2026-04-22 14:19:29 +0100

    Switch colorscheme from zenbones to nightfox
    Replaced 'zenbones.nvim' with 'nightfox.nvim' colorscheme and added configuration.

--- init.lua
+++ init.lua
@@ -141,19 +141,31 @@ require("lazy").setup({
 
   -- Colorscheme
   {
-    "zenbones-theme/zenbones.nvim",
-    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
-    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
-    -- In Vim, compat mode is turned on as Lush only works in Neovim.
-    dependencies = "rktjmp/lush.nvim",
-    lazy = false,
-    priority = 1000,
-    -- you can set set configuration options here
-     config = function()
-    --     vim.g.zenbones_darken_comments = 45
-         vim.cmd.colorscheme('zenbones')
-     end
-},
+  "EdenEast/nightfox.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    -- optional: configure nightfox
+    require("nightfox").setup({
+      -- your options here
+    })
+
+    vim.cmd.colorscheme("dayfox")
+  end,
+  },
+--    "zenbones-theme/zenbones.nvim",
+--    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+--    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+--    -- In Vim, compat mode is turned on as Lush only works in Neovim.
+--    dependencies = "rktjmp/lush.nvim",
+--    lazy = false,
+--    priority = 1000,
+--    -- you can set set configuration options here
+--     config = function()
+--    --     vim.g.zenbones_darken_comments = 45
+--         vim.cmd.colorscheme('zenbones')
+--     end
+-- },
 
 -- Context
 {
